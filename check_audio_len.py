9b7439b7b2614ae9eee0243b114277d8830157bf
Seongmin Lim <gsmlim01@gmail.com>
2022-11-19 23:33:28 +0900

    Update check_audio_len.py

--- check_audio_len.py
+++ check_audio_len.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import sys
 from multiprocessing import Pool
@@ -7,13 +8,14 @@ import numpy as np
 from pymediainfo import MediaInfo
 from tqdm import tqdm
 
+audio_ext_list = ['.aac', '.wav', '.flac', '.m4a', 'mp4', 'mp3', '.ogg']
 
 def get_audio_list(input_dir):
     audio_list = []
     for (_path, _dir, files) in os.walk(input_dir):
         for filename in files:
             ext = os.path.splitext(filename)[-1]
-            if ext == '.wav' or ext == '.flac':
+            if ext.lower() in audio_ext_list:
                 audioname = os.path.join(_path, filename)
                 audio_list.append(audioname)
     return audio_list
@@ -32,25 +34,30 @@ def get_mediainfo_from_file(audiofile):
 
 
 if __name__ == "__main__":
-    input_dir = sys.argv[1]
-    process_num = int(sys.argv[2])
+    parser = argparse.ArgumentParser(description='Check length of audio files from input directory')
+    parser.add_argument('input_dir', type=str, help='Input audio data directory')
+    parser.add_argument('--worker', type=int, help='Number of workers', default=8)
+    args = parser.parse_args()
+
+    input_dir = args.input_dir
 
     audio_list = get_audio_list(input_dir)
-    with Pool(process_num) as p:
+    with Pool(args.worker) as p:
         dict_results = list(tqdm(p.imap(get_mediainfo_from_file, audio_list), total=len(audio_list)))
-    results = [x['dur'] for x in dict_results]
-    sr_results = [x['sr'] for x in dict_results]
-    print(f"Total audiofiles: {len(audio_list)}")
-    none_list = [x for x in results if x == None]
-    dur_list = [x for x in results if x != None]
-    print(f"None: {len(none_list)}")
-    print(f"Not None: {len(dur_list)}")
-    print(f"Total dur: {sum(dur_list)/60:.1f}min")
-    print(f"Total dur: {sum(dur_list)/3600:.1f}hours")
-    print(f"Max dur: {np.max(dur_list):.2f}sec")
-    print(f"Min dur: {np.min(dur_list):.2f}sec")
-    print(f"Mean dur: {np.mean(dur_list):.2f}sec")
+        
+    none_list = [x for x in dict_results if x == None]
+    print(f"Total input audiofiles: {len(audio_list)}")
+    print(f"None results: {len(none_list)}")
+        
+    results_dur = [x['dur'] for x in dict_results if x != None]
+    results_sr = [x['sr'] for x in dict_results if x != None]
+    
+    print(f"Total dur: {sum(results_dur)/60:.1f}min")
+    print(f"Total dur: {sum(results_dur)/3600:.1f}hours")
+    print(f"Max dur: {np.max(results_dur):.2f}sec")
+    print(f"Min dur: {np.min(results_dur):.2f}sec")
+    print(f"Mean dur: {np.mean(results_dur):.2f}sec")
     sr_dict = defaultdict(int)
-    for sr in sr_results:
+    for sr in results_sr:
         sr_dict[sr] += 1
     print(f"Sampling rates: {dict(sr_dict)}")
